418af7d73257db3ee2d8cd6241b12146380f6a6e
Tin Tomašić <tin.tomasic74@gmail.com>
2021-03-04 23:19:20 +0100

    added project lombok

--- src/main/java/bee/entity/Info.java
+++ src/main/java/bee/entity/Info.java
@@ -1,25 +1,8 @@
 package bee.entity;
 
-public class Info {
-	private int beehives;
-	private String honeyType, place;
+import lombok.Data;
 
-	public Info(int beehives, String honeyType, String place) {
-		this.beehives = beehives;
-		this.honeyType = honeyType;
-		this.place = place;
-	}
-
-	public int getBeehives() {
-		return beehives;
-	}
-
-	public String getHoneyType() {
-		return honeyType;
-	}
-
-	public String getPlace() {
-		return place;
-	}
-
+public @Data class Info {
+	private final int beehives;
+	private final String honeyType, place;
 }
--- src/main/java/bee/entity/Scale.java
+++ src/main/java/bee/entity/Scale.java
@@ -1,25 +1,9 @@
 package bee.entity;
 
-public class Scale {
-	private int day;
-	private double value, status;
+import lombok.Data;
 
-	public Scale(int day, double value, double status) {
-		this.day = day;
-		this.value = value;
-		this.status = status;
-	}
-
-	public int getDay() {
-		return day;
-	}
-
-	public double getValue() {
-		return value;
-	}
-
-	public double getStatus() {
-		return status;
-	}
+public @Data class Scale {
+	private final int day;
+	private final double value, status;
 
 }
